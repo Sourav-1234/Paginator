53d84b5b25d5e774d531a5081b7382ab405d747b
intern2_crosscut <sourav024saha@gmail.com>
2024-02-26 00:58:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import logo from './logo.svg';
+import './App.css';
+import {useEffect,useState} from 'react';
+
+function App() {
+  const [products,setProducts]=useState([]);
+  const [pages,setPages]=useState(1);
+
+  const fetchProducts =async() =>{
+    const res =await fetch("https://dummyjson.com/products?limit=100")
+     
+    const data =await res.json();
+    
+    if(data && data.products){
+      setProducts(data.products)
+    }
+
+
+
+    console.log(data);
+  };
+
+  useEffect(() =>{
+    fetchProducts()
+  },[])
+
+const selectPageHandler=(selectedPage)=>{
+ setPages(selectedPage);
+}
+
+
+  return (
+    <div >
+     {
+      products.length>0 && <div className="products">
+      {
+        products.slice(pages*10-10,pages*10).map((prod)=>{
+          return( <span className="products__single" key={prod.id}>
+           <img src={prod.thumbnail}  alt={prod.title} />
+           <span>{prod.title}</span>
+          </span>);
+        })}
+      </div>
+     }
+     {
+      products.length>0 &&<div className="pagination">
+      <span  onClick={()=>selectPageHandler(pages-1)}></span>
+      <span>🤛</span>
+      {
+        [...Array(products.length /10)].map((_,  i)=>{
+
+           return (
+            <span className={pages === i+1?"pagination__selected":""}
+            onClick={()=>selectPageHandler(i+1)} key={i}>{i+1}</span>);
+
+         })
+      }
+      
+      <span onClick={()=>selectPageHandler(pages+1)}>🤜</span>
+      
+      </div>
+     }
+    </div>
+  );
+}
+
+export default App;
